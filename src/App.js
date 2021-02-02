9baeb4e334df5752a94cb2773320db512cd8a1c6
ianhutson <ih00209@georgiasouthern.edu>
2021-02-02 13:14:32 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = event => {
+    this.props.addItem()
   }
 
   render() {
@@ -24,6 +24,14 @@ class App extends Component {
 const mapStateToProps = (state) => {
   return {
     items: state.items
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addItem())
+    }
   };
 };
 
